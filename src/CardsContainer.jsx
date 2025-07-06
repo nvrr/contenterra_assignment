63c8337c00381233c0bf209fc04404026b0febe5
nvrr <sureshvarma809@gmail.com>
2025-07-06 22:37:13 +0530

    css issue

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -30,7 +30,7 @@ export function CardsContainer () {
 
   return (
     <div className=" bg-white text-gray-600 min-h-screen">
-      
+      <h1 className="text-4xl text-neutral-400 mb-10 font-bold">ContententErra-Assignment</h1>
       <div className="max-w-[1280px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
           {posts.map((post) => (
             <CardUi
