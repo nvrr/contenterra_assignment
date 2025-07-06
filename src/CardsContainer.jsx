2ec5c3edcbf40e6de8e14afc139915e88734c772
nvrr <sureshvarma809@gmail.com>
2025-07-06 21:58:01 +0530

    first commit

---
+++ src/CardsContainer.jsx
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function CardsContainer () {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("https://www.reddit.com/r/reactjs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const items = data.data.children.map((item) => item.data);
+        setPosts(items);
+      })
+      .catch((err) => console.error("Failed to fetch Reddit posts:", err))
+      .finally(
+        setLoading(false)
+      )
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="bg-yellow-50 w-screen h-screen flex items-center justify-center text-3xl text-blue-600">
+      please wait...
+      </div>
+    );
+  }
+
+  return (
+    <div className=" bg-white text-gray-600 min-h-screen">
+      
+      <div className="max-w-[1280px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
+          {posts.map((post) => (
+            <CardUi
+              key={post.id}
+              title={post.title}
+              selftextHtml={post.selftext_html}
+              score={post.score}
+              url={post.permalink}
+            />
+          ))}
+       </div>
+      </div>
+    
+  );
+}
+
+
+const CardUi = ({ title, selftextHtml, score,url }) => {
+  const decodeHtmlEntities = (html) => {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = html;
+    return textArea.value;
+  };
+  const decodedHtml = decodeHtmlEntities(selftextHtml);
+
+  return (
+    <div className=" border-4 border-gray-300 bg-yellow-200 rounded-xl  p-4 flex flex-col justify-between h-full">
+      <div>
+        <h5 className="text-lg font-semibold mb-2 text-gray-800">{title}</h5>
+       {decodedHtml ? <div
+          className="text-sm text-gray-600 mb-3"
+          dangerouslySetInnerHTML={{ __html: decodedHtml }}
+        /> : <div className="flex-1 flex items-center justify-center bg-red-400 text-white text-sm rounded p-4">No Content</div>}
+      </div>
+      
+      <div className="mt-auto">
+      <hr/>
+        <small className="flex flex-col text-gray-500 mb-2">Score: {score}</small>
+        <a
+          href={`https://www.reddit.com${url}`}
+          target="_blank"
+          className="text-blue-600 hover:underline text-sm"
+        >
+          View Post
+        </a>
+      </div>
+    </div>
+  );
+};
+
